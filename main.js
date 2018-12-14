15cb0d052ebeb2d47309fc4775f7b53ce5fa4c7a
Darryl Williams <kirbydinaverg@gmail.com>
2018-12-14 17:58:27 -0600

    translation needs apikey

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict'
 
-let url = "https://translation.googleapis.com/language/translate/v2"
+let searchURL = "https://translation.googleapis.com/language/translate/v2"
+let apiKey = "AIzaSyC6dqpJDXX_teG-8LF5k_v8lAqgD9ICxuk"
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -13,5 +14,39 @@ function watchForm() {
         event.preventDefault();
         const text = $("#translateThis").val()
         const target = $("#target").val()
+        translate(text, target)
     })
 }
+
+function translate(text, target) {
+    const params = {
+        q: text,
+        target: target,
+        key: apiKey,
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+
+    fetch(url, {
+        method: 'POST',
+        body: {
+            q: text
+        },
+        headers: new Headers()
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+} 
+
+
+
+
+$(watchForm)
